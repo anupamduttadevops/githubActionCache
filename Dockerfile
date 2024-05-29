15e66bea9157b2347017c95ab86f2fbbc17f8626
Anupam Dutta <techverito@TechVeritos-MacBook-Pro.local>
2024-05-29 15:51:09 +0530

    test

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /app
 
 COPY go.mod go.sum ./
 
-RUN go get -d -v ./...
+RUN go mod download
 
 COPY . .
 
--- main.go
+++ main.go
@@ -7,6 +7,20 @@ import (
 	_ "github.com/go-redis/redis/v8"
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
+
+	// Additional dependencies
+	_ "github.com/aws/aws-sdk-go"
+	_ "github.com/gin-gonic/gin"
+	_ "github.com/go-playground/validator"
+	_ "github.com/googleapis/gax-go/v2"
+	_ "github.com/gorilla/websocket"
+	_ "github.com/grpc-ecosystem/go-grpc-middleware"
+	_ "github.com/jinzhu/gorm"
+	_ "github.com/joho/godotenv"
+	_ "github.com/prometheus/client_golang/prometheus"
+	_ "github.com/sirupsen/logrus"
+	_ "github.com/spf13/viper"
+	_ "golang.org/x/image/draw"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
